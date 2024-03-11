af335d2ac4c6067ad37cc7fbb8203e49c9ff7202
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-03-11 11:04:39 +0530

    update gmail credentials

--- utils/nodemailer.js
+++ utils/nodemailer.js
@@ -2,8 +2,7 @@ require("dotenv").config();
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
-  port: 587,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
